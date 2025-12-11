966355cedc42654d554a98b1b17df767d7c20574
AmDeeJhay <amdeejhay@gmail.com>
2025-12-11 21:31:35 +0100

    feat: restore BASE_URL definition to use dynamic environment variable for API base URL

--- src/utils/authApi.js
+++ src/utils/authApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://api.swiftlyxpress.com';
-const BASE_URL = 'https://api.swiftlyxpress.com';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://api.swiftlyxpress.com';
+// const BASE_URL = 'https://api.swiftlyxpress.com';
 
 
 const apiClient = axios.create({
